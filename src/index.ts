2319385c42708e12cf6d1b13a29fb12dd86ec655
Leandro Herenu <lherenu@coalfire.com>
2021-05-18 17:56:25 -0300

    adding generateRestAPI

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import express from "express";
+
 export interface Option {
   val: number;
   desc: string;
@@ -14,3 +16,41 @@ export interface RestService<T> {
   put(item: T);
   options(): Option[] | Promise<Option[]>;
 }
+
+export function generateRestAPI<T>(service: RestService<T>) {
+  const route = express.Router();
+
+  route
+    .route("/")
+    .get(async (req, res, next) => {
+      const items = await service.getAll();
+
+      res.send(items);
+    })
+    .post(async (req, res, next) => {
+      const result = await service.post(req.body);
+
+      res.send(result);
+    });
+
+  route.route("/options").get(async (req, res, next) => {
+    res.send(await service.options());
+  });
+
+  route
+    .route("/:id")
+    .get(async (req, res, next) => {
+      const item = await service.get(+req.params.id);
+
+      res.send(item);
+    })
+    .put(async (req, res, next) => {
+      const id = +req.params.id;
+
+      const result = await service.put({ id, ...req.body });
+
+      res.send(result);
+    });
+
+  return route;
+};
